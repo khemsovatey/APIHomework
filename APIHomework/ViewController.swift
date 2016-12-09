6e8a9ad6f53ad0c6dbf74c1cf6b34d0853063907
ខែម សុវត្តី <khemsovatey@MacBooks-MacBook-Pro-5.local>
2016-12-09 14:03:34 +0700

    Initial Commit

---
+++ APIHomework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  APIHomework
+//
+//  Created by ខែម សុវត្តី on 12/9/16.
+//  Copyright © 2016 ខែម សុវត្តី. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
